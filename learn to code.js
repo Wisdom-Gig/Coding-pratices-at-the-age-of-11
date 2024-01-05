1e352b9dd06aa9c5573175786d5f93003212196a
Aderinto Wisdom <wisom.aderinto@gmail.com>
2024-01-05 10:52:53 +0100

    11th commit

--- learn to code.js
+++ learn to code.js
@@ -1,8 +1,32 @@
 
-var name = " Wisdom ";
-var age = " 12 ";
+// var name = " Wisdom ";
+// var age = " 12 ";
+// var shootingScore = 45.6;
 
-var message = "Hi, my name is " + name + "and I am" + age + "years old!";
+// var message = "Hi, my name is " + name + "and I am" + age + "years old!";
 
 
- console.log(message)
+// var firstName = "Wisdom";
+// var lastName = "Aderinto";
+// var dateOfBirth = "24-01-12";
+// var age = "12";
+// var profileImgUrl = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatisticsbyjim.com%2Fwp-content%2Fuploads%2F2019%2F05%2Frandom_dice.png&tbnid=t7xDOQ04MaHJHM&vet=12ahUKEwiAz_mj8sODAxWguCcCHdoVCXsQMygOegUIARCRAQ..i&imgrefurl=https%3A%2F%2Fstatisticsbyjim.com%2Fbasics%2Frandom-assignment-experiments%2F&docid=hbuItVgOEHdEdM&w=600&h=471&q=random%20pictures&ved=2ahUKEwiAz_mj8sODAxWguCcCHdoVCXsQMygOegUIARCRAQ";
+
+// var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + " rdbirthday!" ;
+
+ //console.log(loginWelcomeMessage)
+
+ var sum = 10 + 15;
+ var sub = 10 - 15;
+ var mul = 10 * 15;
+ var div = 10 / 3;
+ var mod = 10 % 3;
+
+ var msg = "10 / 3 = 3 with a remainder of " + mod
+
+
+var result = 10 * ((5 + 3) - 4);
+
+
+ console.log(result)
+ 
