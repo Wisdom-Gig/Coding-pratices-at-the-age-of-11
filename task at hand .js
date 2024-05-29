cee0abb89e94ccca78a6b483b5a606146fa5782c
Aderinto Wisdom <wisom.aderinto@gmail.com>
2024-05-29 21:22:23 +0100

    18thcommit

---
+++ task at hand .js
@@ -0,0 +1,18 @@
+var FirstBank = "5,000,000,000,000";
+var AccessBank = "120,000,000,000,000";
+var GtBank ="40,000,000,000,000";
+
+var balances = ["FirstBank", "AccessBank", "GtBank"];
+
+var broke = [];
+broke.push(balances[0]);
+
+var index = broke.indexOf("FirstBank")
+
+console.log(broke)
+
+if (index > -1) {
+    broke.splice(index, 1)
+}
+
+console.log(broke)
